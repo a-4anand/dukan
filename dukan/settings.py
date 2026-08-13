af26c11b5c0537070c9a89bbf16c070fd5edb67e
Anand kumar dubey <142786893+a-4anand@users.noreply.github.com>
2026-08-13 19:10:12 +0530

    fix: added MEDIA_URL to prevent catchall URL intercepting missing slashes

--- dukan/settings.py
+++ dukan/settings.py
@@ -88,6 +88,9 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "dabeli", "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 JAZZMIN_SETTINGS = {
